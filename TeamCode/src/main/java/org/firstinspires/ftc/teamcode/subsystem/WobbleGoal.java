ee8dfa769f6c17bc6f991439572d25ee32562be8
Joseph-Campos <57970693+Joseph-Campos@users.noreply.github.com>
2021-01-15 15:21:48 -0800

    Update WobbleGoal.java
    A start

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/WobbleGoal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/WobbleGoal.java
@@ -1,18 +1,43 @@
 package org.firstinspires.ftc.teamcode.subsystem;
 
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
-
+// Joseph Campos Date: 1/15/2021
 public class WobbleGoal {
     // Characteristics
     private DcMotor wobbleLift;
-    private Servo wobbleGrabber,wobbleGrabber2;
-
+    private CRServo wobbleGrabber,wobbleGrabber2;
     // Constructor
-    public WobbleGoal(DcMotor WL, Servo WG1, Servo WG2){
+
+    public WobbleGoal(DcMotor WL, CRServo WG1, CRServo WG2){
         wobbleLift = WL;
         wobbleGrabber = WG1;
         wobbleGrabber2 = WG2;
+    }
+
+    //WobbleLift going up
+    public void up(double power){
+        wobbleLift.setPower(power);
+    }
+
+    //WobbleLift going down
+    public void down(double power){
+        wobbleLift.setPower(-power);
+    }
+
+    //Wobblegrabber close
+
+    public void grab(double power){
+        wobbleGrabber.setPower(power);
+        wobbleGrabber2.setPower(-power);
+    }
+
+    //Wobblegrabber open
+
+    public void Drop(double power){
+        wobbleGrabber.setPower(-power);
+        wobbleGrabber2.setPower(power);
     }
 
 
