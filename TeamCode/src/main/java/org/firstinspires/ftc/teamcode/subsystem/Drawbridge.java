1c3e4ea5526fd9ac34a95a261956949bdd4c9817
Jacob Marinas <46766490+JayZeeKay@users.noreply.github.com>
2021-01-28 17:47:34 -0800

    Wrote Drawbridge, Intake subsystem; added javadoc comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Drawbridge.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Drawbridge.java
@@ -2,7 +2,56 @@ package org.firstinspires.ftc.teamcode.subsystem;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+/**
+ * Date: 1/28/21
+ * Author: Jacob Marinas
+ * The drawbridge subsystem
+ */
 public class Drawbridge {
 
     private DcMotor pulleyBoi;
+    private double power;
+
+    /**
+     * Defines the parts needed for the subsystem
+     * @param pB
+     */
+    public Drawbridge(DcMotor pB) {
+        this.pulleyBoi = pB;
+    }
+
+    /**
+     * Brings the drawbridge mechanism up
+     */
+    public void up() {
+        pulleyBoi.setPower(-power);
+    }
+
+    /**
+     * Brings the drawbridge mechanism down
+     */
+    public void down() {
+        pulleyBoi.setPower(power);
+    }
+
+    /**
+     * Stops the drawbridge mchanism
+     */
+    public void stop() {
+        pulleyBoi.setPower(0);
+    }
+
+    /**
+     * @param power that the drawbridge will operate by
+     */
+    public void setPower(double power) {
+        this.power = power;
+    }
+
+    /**
+     * @return power that the drawbridge operates by
+     */
+    public double getPower() {
+        return power;
+    }
 }
